06ef051d05a4e14fedd810458f1357953c5d7603
!  Am Chill Guy <104963991+AmJustAChillGuy@users.noreply.github.com>
2025-03-06 18:52:24 +0700

    Update commands.jsx

--- pages/commands.jsx
+++ pages/commands.jsx
@@ -2,60 +2,59 @@ import { Menu, Transition } from "@headlessui/react";
 import Link from "next/link";
 
 export default function Commands() {
-const commands = [
-  "/autoplay",
-  "/back",
-  "/statistic",
-  "/channel",
-  "/clear",
-  "/dj",
-  "/filter",
-  "/help",
-  "/language",
-  "/loop",
-  "/nowplaying",
-  "/pause",
-  "/ping",
-  "/play",
-  "/playlist",
-  "/queue",
-  "/resume",
-  "/save",
-  "/search",
-  "/seek",
-  "/servers",
-  "/shuffle",
-  "/skip",
-  "/stop",
-  "/time",
-  "/volume"
-];
+  const commands = [
+    "/autoplay",
+    "/back",
+    "/statistic",
+    "/channel",
+    "/clear",
+    "/dj",
+    "/filter",
+    "/help",
+    "/language",
+    "/loop",
+    "/nowplaying",
+    "/pause",
+    "/ping",
+    "/play",
+    "/playlist",
+    "/queue",
+    "/resume",
+    "/save",
+    "/search",
+    "/seek",
+    "/servers",
+    "/shuffle",
+    "/skip",
+    "/stop",
+    "/time",
+    "/volume"
+  ];
 
   return (
     <>
       <div>
-      <div className="flex w-full justify-center">
-      <img width="500" src="/img/bck2.png" />
-      </div>
+        <div className="flex w-full justify-center">
+          <img width="500" src="/img/bck2.png" />
+        </div>
         <p className="text-xl font-medium text-white">
           <i className="fal fa-cogs text-amber-400 mr-2" />
-         Bot Commands
+          AsTrazu Commands
         </p>
         <p className="text-white text-sm text-opacity-50 mb-5">
-        You can get information about the commands of the MusicMaker Bot.
+          Your go-to list for all the cool things AsTrazu can do. Play your jams, tweak
+          settings, and vibe in style.
         </p>
 
         <div>
           <Menu as="div" className="w-full relative inline-block text-left">
-          <div>
+            <div>
               <Menu.Button className="left-0 transition w-full duration-200 hover:bg-opacity-50 bg-black bg-opacity-30 text-white focus:text-amber-400 rounded-xl border border-white/10 focus:border-amber-400 focus:outline-none py-4 px-6 flex justify-between items-center">
                 <div className="flex items-center gap-x-1 text-white text-opacity-60">
-                 
-                    <>
-                      <i className="fa fa-hashtag text-white text-opacity-20" />
-                     View Commands List
-                    </>
-                
+                  <>
+                    <i className="fa fa-hashtag text-white text-opacity-20" />
+                    View Commands List
+                  </>
                 </div>
               </Menu.Button>
             </div>
@@ -66,51 +65,51 @@ const commands = [
               enterTo="transform opacity-100 scale-100"
               leave="transition ease-in duration-75"
               leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95">
+              leaveTo="transform opacity-0 scale-95"
+            >
               <Menu.Items className="z-1 custom-scroll absolute left-0 w-full mt-2 origin-top-right bg-black overflow-auto max-h-60 border border-black border-opacity-20 bg-opacity-95 rounded-lg p-1">
                 <div className="px-1 py-1 w-full">
-
-            {commands.map(mm => (
-
-                <Menu.Item>
-                    <button className={`w-full rounded-lg p-2 cursor-pointer text-white flex items-center space-x-3 transition-all duration-200`}>
-                    <>
+                  {commands.map((cmd, index) => (
+                    <Menu.Item key={index}>
+                      <button className="w-full rounded-lg p-2 cursor-pointer text-white flex items-center space-x-3 transition-all duration-200">
+                        <>
                           <i className="fa-spin text-xl text-white text-opacity-20" />
-                         {mm}</>
-                    </button>
+                          {cmd}
+                        </>
+                      </button>
                     </Menu.Item>
-            ))}
-              
-        </div>
+                  ))}
+                </div>
               </Menu.Items>
             </Transition>
           </Menu>
         </div>
 
         <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
-  
-  <Link href={"https://discord.com/api/oauth2/authorize?client_id=774043716797071371&permissions=277028620608&scope=applications.commands%20bot"}>
-  <a className={"flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Invite MusicMaker</a>
-  </Link>
+          <Link href="https://discord.com/oauth2/authorize?client_id=698817988190535720">
+            <a className="flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white">
+              Invite AsTrazu
+            </a>
+          </Link>
 
-  <div className="py-10"></div>
-  
-  <Link href={"https://discord.gg/ACJQzJuckW"}>
-  <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Support Server</a>
-  </Link>
+          <div className="py-10"></div>
 
-  <div className="py-10"></div>
-  
-  <Link href={"https://top.gg/bot/774043716797071371/vote"}>
-  <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Vote MusicMaker</a>
-  </Link>
-</div>
+          <Link href="https://discord.gg/ACJQzJuckW">
+            <a className="px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white">
+              Support Server
+            </a>
+          </Link>
 
+          <div className="py-10"></div>
+
+          <Link href="https://top.gg/bot/698817988190535720/vote">
+            <a className="px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white">
+              Vote AsTrazu
+            </a>
+          </Link>
+        </div>
       </div>
       <div className="py-20"></div>
     </>
-  )
-  }
+  );
+}
