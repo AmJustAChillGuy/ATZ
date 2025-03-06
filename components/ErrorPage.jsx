72ffe5b2ac552992ad7716080743394cb9df8347
!  Am Chill Guy <104963991+AmJustAChillGuy@users.noreply.github.com>
2025-03-06 19:02:02 +0700

    Update ErrorPage.jsx

--- components/ErrorPage.jsx
+++ components/ErrorPage.jsx
@@ -5,29 +5,39 @@ import { useState } from "react";
 const ErrorPage = ({ code, message }) => {
     const [clicked, setClicked] = useState(false);
     const router = useRouter();
-    
-    return <>
-        <Head>
-            <title>{code} | MusicMaker</title>
-        </Head>
 
-        <div className="max-w-7xl text-center py-20 mx-auto">
-        <div className="flex w-full justify-center">
-      <img width="400" src="/img/bck3.png" />
-      </div>
-            <h1 className="text-4xl font-extrabold text-white">{code}</h1>
-            <p className="text-xl font-thin text-white text-opacity-75">{message}</p>
-            <button
-                className="w-42 shadow-lg mt-2 shadow-amber-600/20 rounded-xl py-2 font-medium px-7 text-zinc-900 bg-amber-400 hover:bg-opacity-50 transition duration-200"
-                onClick={() => {
-                    setClicked(true);
-                    router.back();
-                }}
-            >
-                {clicked ? <i className="fad fa-spinner-third fa-spin text-white" /> : <><i className='fa fa-arrow-left mr-2' />Go Back</>}
-            </button>
-        </div>
-    </>
-}
+    return (
+        <>
+            <Head>
+                <title>{code} | AsTrazu</title>
+            </Head>
+
+            <div className="max-w-7xl text-center py-20 mx-auto">
+                <div className="flex w-full justify-center">
+                    <img width="400" src="/img/error.png" alt="Error Image" />
+                </div>
+                <h1 className="text-5xl font-extrabold text-white drop-shadow-lg">{code}</h1>
+                <p className="text-lg font-light text-white/70 mt-2">{message}</p>
+                
+                <button
+                    className="mt-4 px-6 py-2 font-medium text-zinc-900 bg-amber-400 rounded-xl shadow-lg hover:bg-opacity-50 transition duration-200"
+                    onClick={() => {
+                        setClicked(true);
+                        router.back();
+                    }}
+                >
+                    {clicked ? (
+                        <i className="fad fa-spinner-third fa-spin text-white" />
+                    ) : (
+                        <>
+                            <i className="fa fa-arrow-left mr-2" />
+                            Take Me Back
+                        </>
+                    )}
+                </button>
+            </div>
+        </>
+    );
+};
 
 export default ErrorPage;
