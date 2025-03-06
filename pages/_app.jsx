4499a4493a238e95515db561a346abeb33016f95
!  Am Chill Guy <104963991+AmJustAChillGuy@users.noreply.github.com>
2025-03-06 18:58:17 +0700

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,20 +3,19 @@ import "../public/css/tippy.css";
 import "../public/css/customColors.css";
 import "tailwindcss/tailwind.css";
 import NProgress from "nprogress";
-import Router, { useRouter } from "next/router";
+import Router from "next/router";
 import Head from "next/head";
 
 import Header from "../components/Static/Header.jsx";
 import Footer from "../components/Static/Footer.jsx";
+import { ThemeProvider } from "next-themes";
 
-Router.onRouteChangeStart = () => NProgress.start();
-Router.onRouteChangeComplete = () => NProgress.done();
-Router.onRouteChangeError = () => NProgress.done();
+// Page loading animation
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
-import { ThemeProvider } from 'next-themes'
-
-export default function AwardApp({ Component, pageProps }) {
- 
+export default function AsTrazuApp({ Component, pageProps }) {
   const NavItems = [
     {
       link: true,
@@ -41,7 +40,7 @@ export default function AwardApp({ Component, pageProps }) {
     },
     {
       link: true,
-      name: "Add Bot",
+      name: "Invite Bot",
       icon: "fal fa-robot",
       activeIcon: "fab fa-robot",
       href: "https://discord.com/api/oauth2/authorize?client_id=774043716797071371&permissions=277028620608&scope=bot%20applications.commands",
@@ -52,34 +51,32 @@ export default function AwardApp({ Component, pageProps }) {
       icon: "fal fa-handshake",
       activeIcon: "fa fa-handshake",
       href: "/partners",
-    }
-  ]
+    },
+  ];
 
   return (
-    <ThemeProvider defaultTheme='violet'>
-    <div className="h-screen relative border-t-4 border-amber-600">
-      <div
-        className="bg-gradient-to-b z-10 opacity-[25%] absolute top-0 w-full from-amber-600 to-transparent"
-        style={{ height: "500px" }}
-      />
-      <Head>
-        <title>
-          MusicMaker
-        </title>
-      </Head>
-      <main className="transition-all duration-200 z-10 absolute inset-0 px-5 h-screen max-w-7xl w-full mx-auto">
-        <Header NavItems={NavItems} />
-        <div className="block px-5 md:px-0">
-          <Component {...pageProps} />
+    <ThemeProvider defaultTheme="dark">
+      <div className="h-screen relative border-t-4 border-amber-600">
+        <div
+          className="bg-gradient-to-b z-10 opacity-25 absolute top-0 w-full from-amber-600 to-transparent"
+          style={{ height: "500px" }}
+        />
+        <Head>
+          <title>AsTrazu - Chill Vibes</title>
+        </Head>
+        <main className="transition-all duration-200 z-10 absolute inset-0 px-5 h-screen max-w-7xl w-full mx-auto">
+          <Header NavItems={NavItems} />
+          <div className="block px-5 md:px-0">
+            <Component {...pageProps} />
+          </div>
+          <Footer />
+        </main>
+        <div>
+          <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js" />
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js" />
+          <script src="/js/main.js?i=2" />
         </div>
-        <Footer />
-      </main>
-      <div>
-        <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js" />
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js" />
-        <script src="/js/main.js?i=2" />
       </div>
-    </div>
     </ThemeProvider>
   );
 }
